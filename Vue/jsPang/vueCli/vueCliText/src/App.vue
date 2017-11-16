a7882fc129522e13812f2f1110bd5e2ea7831cc3
Fangzhu-Tianji <zch793262201@163.com>
2017-11-16 13:51:09 +0800

    add

--- Vue/jsPang/vueCli/vueCliText/src/App.vue
+++ Vue/jsPang/vueCli/vueCliText/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">首页</router-link>
+      <router-link to="/hi">hi页面</router-link>
+    </div>
+
     <router-view/>
   </div>
 </template>
---
+++ Vue/jsPang/vueCli/vueCliText/src/components/hi.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hi',
+  data () {
+    return {
+      msg: 'hi 欢迎光临！！'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
