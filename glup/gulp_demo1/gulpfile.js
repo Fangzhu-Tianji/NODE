5dc40751a82cb6d118b51bf1f2f41252df39a415
Fangzhu-Tianji <zch793262201@163.com>
2017-11-21 19:12:25 +0800

    add

---
+++ glup/gulp_demo1/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+      less = require('gulp-less');
+
+gulp.task('testless', function() {
+    gulp.src('src/less/index.less')
+           .pipe(less())
+           .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('default', ['testless']);
