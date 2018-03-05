f415b98c0606c89cf4148ed0062715c639c10c08
Fangzhu-Tianji <zch793262201@163.com>
2018-03-05 13:31:29 +0800

    add

--- Vue/jsPang/awesomePOS/src/components/page/Pos.vue
+++ Vue/jsPang/awesomePOS/src/components/page/Pos.vue
@@ -151,28 +151,35 @@ export default {
             type0Goods:[],
             type1Goods:[],
             type2Goods:[],
-            type3Goods:[]
+            type3Goods:[],
+            oftenGoodsJson: [{"goodsId":1,"goodsName":"香辣鸡腿堡","price":18},{"goodsId":2,"goodsName":"田园鸡腿堡","price":15},{"goodsId":3,"goodsName":"和风汉堡","price":15},{"goodsId":4,"goodsName":"大包薯条","price":18},{"goodsId":5,"goodsName":"脆皮炸鸡腿","price":20},{"goodsId":6,"goodsName":"魔法鸡块","price":20},{"goodsId":7,"goodsName":"可乐大杯","price":10},{"goodsId":8,"goodsName":"雪顶咖啡","price":18},{"goodsId":9,"goodsName":"儿童欢乐套餐","price":25},{"goodsId":10,"goodsName":"快乐全家桶","price":99}],
+            typeGoodsJosn: [[{"goodsId":1,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg","goodsName":"香辣鸡腿堡","price":18},{"goodsId":2,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg","goodsName":"田园鸡腿堡","price":15},{"goodsId":3,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg","goodsName":"和风汉堡","price":15}],[{"goodsId":4,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg","goodsName":"大包薯条","price":18},{"goodsId":5,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg","goodsName":"脆皮炸鸡腿","price":20},{"goodsId":6,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg","goodsName":"魔法鸡块","price":20}],[{"goodsId":7,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg","goodsName":"可乐大杯","price":10},{"goodsId":8,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg","goodsName":"雪顶咖啡","price":18}],[{"goodsId":9,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg","goodsName":"儿童欢乐套餐","price":25},{"goodsId":10,"goodsImg":"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg","goodsName":"快乐全家桶","price":99}]]
         }
     },
     created ()  {
         // axios请求数据
-        axios.get('http://jspang.com/DemoApi/oftenGoods.php')
-        .then(response => {
-            this.oftenGoods = response.data;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-        axios.get('http://jspang.com/DemoApi/typeGoods.php')
-        .then(response => {
-            this.type0Goods = response.data[0];
-            this.type1Goods = response.data[1];
-            this.type2Goods = response.data[2];
-            this.type3Goods = response.data[3];
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        // axios.get('http://jspang.com/DemoApi/oftenGoods.php')
+        // .then(response => {
+        //     this.oftenGoods = response.data;
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        // });
+        // axios.get('http://jspang.com/DemoApi/typeGoods.php')
+        // .then(response => {
+        //     this.type0Goods = response.data[0];
+        //     this.type1Goods = response.data[1];
+        //     this.type2Goods = response.data[2];
+        //     this.type3Goods = response.data[3];
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        // });
+        this.oftenGoods = this.oftenGoodsJson;
+        this.type0Goods = this.typeGoodsJosn[0];
+        this.type1Goods = this.typeGoodsJosn[1];
+        this.type2Goods = this.typeGoodsJosn[2];
+        this.type3Goods = this.typeGoodsJosn[3];
         if(localStorage.getItem('putList')) {
             this.tableGua = JSON.parse(localStorage.getItem('putList'));
         }
