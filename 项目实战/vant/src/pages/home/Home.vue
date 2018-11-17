64f9dbcf4c756f7f6f8955dd69cd2978665d7d5f
Fangzhu-Tianji <zch793262201@163.com>
2018-11-17 19:52:16 +0800

    add vant

---
+++ 项目实战/vant/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+    <div class="van-hairline--top"></div>
+    <div class="van-ellipsis">这是一段宽度限制 250px 的文字，后面的内容会省略的文字，后面的内容会省略的文字，后面的内容会省略</div>
+  </div>
+</template>
+
+<script>
+import { Button } from 'vant'
+export default {
+  name: 'Home',
+  components: {
+    [Button.name]: Button
+  }
+}
+</script>
+
